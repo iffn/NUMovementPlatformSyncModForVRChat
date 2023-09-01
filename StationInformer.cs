e1bea327e184b5afb880aabc9c0a6ffbddd661a2
iffn <iffn1990@gmail.com>
2023-09-01 16:48:34 +0200

    Added system to use other stations

---
+++ StationInformer.cs
@@ -0,0 +1,25 @@
+﻿
+using Nessie.Udon.Movement;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class StationInformer : UdonSharpBehaviour
+{
+    [SerializeField] NUMovement linkedNuMovement;
+
+    public override void OnStationEntered(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        linkedNuMovement._ControllerDisable();
+    }
+
+    public override void OnStationExited(VRCPlayerApi player)
+    {
+        if (!player.isLocal) return;
+
+        linkedNuMovement._ControllerEnable();
+    }
+}
