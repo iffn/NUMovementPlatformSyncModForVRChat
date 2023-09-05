438fe4a2f277c35b32708e31542ebf8dbb610571
iffn <iffn1990@gmail.com>
2023-09-05 20:40:19 +0200

    Readded for upper case name change

---
+++ Utilities/MovementModLinker.cs
@@ -0,0 +1,20 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace NUMovementPlatformSyncMod
+{
+    public class MovementModLinker : UdonSharpBehaviour
+    {
+        [SerializeField] NUMovementSyncMod linkedMovementMod;
+
+        public NUMovementSyncMod LinkedMovementMod
+        {
+            get
+            {
+                return linkedMovementMod;
+            }
+        }
+    }
+}
